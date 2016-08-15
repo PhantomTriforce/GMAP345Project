ee6f2526549a54b36e45f39b51d2e8cc5b1e1f7c
PhantomTriforce <phantomtriforce@yahoo.com>
2016-08-15 12:25:01 -0400

    added scrolling the zoom in/out camera

---
+++ Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	float zoomAmount = 0f; //With Positive and negative values
+	float maxToClamp = 10f;
+	float ROTSpeed = 10f;
+
+	void Update() {
+		zoomAmount += Input.GetAxis("Mouse ScrollWheel");
+		zoomAmount = Mathf.Clamp(zoomAmount, -maxToClamp, maxToClamp);
+		float translate = Mathf.Min(Mathf.Abs(Input.GetAxis("Mouse ScrollWheel")), maxToClamp - Mathf.Abs(zoomAmount));
+		gameObject.transform.Translate(0,0,translate * ROTSpeed * Mathf.Sign(Input.GetAxis("Mouse ScrollWheel")));
+	}
+}
