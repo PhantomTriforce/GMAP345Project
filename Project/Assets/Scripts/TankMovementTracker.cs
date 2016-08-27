3128b6eadbcc6832c0fa8a0d68c715cdf23e0c29
HardTacos <thehardtacos@gmail.com>
2016-08-26 21:12:49 -0400

    fuck boi

---
+++ Project/Assets/Scripts/TankMovementTracker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMovementTracker : MonoBehaviour {
+
+    public float maxDistance = 50f;
+    public float totalDistance = 0f;
+    Vector3 lastPosition;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        totalDistance += Vector3.Distance(transform.position, lastPosition);
+        lastPosition = transform.position;
+    }
+}
